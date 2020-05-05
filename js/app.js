7d8cd969f249ad8ec56dfea3f432bcb714cabc7a
VPJose <josegregoriovp@gmail.com>
2020-05-05 03:13:57 -0400

    Se agregaron efectos a las imagenes

--- js/app.js
+++ js/app.js
@@ -5,35 +5,39 @@ var resultado = document.getElementById('resultado');
 formulaio.addEventListener('submit', e => {
   e.preventDefault();
   var bsq = document.querySelector('#nani').value;
-  var respuesta = document.getElementById('respuesta');
 
+  fetch(` https://api.jikan.moe/v3/search/anime?q=${bsq}&limit=30`)
+  .then( res => {
+    return res.json();
+  })
+  .then(res => {
+    mostrar(res.results);
+  })
+  .catch( err =>  alert( "Error: " + err) );
+});
+
+function mostrar(res) {
+  console.log(res[0]);
+  var respuesta = document.getElementById('respuesta');
 
   while (respuesta) {
     resultado.removeChild(respuesta);
     var respuesta = document.getElementById('respuesta');
   }
 
-  fetch(`https://api.jikan.moe/v3/search/anime?q=${bsq}&limit=18`)  // &order_by=title, no quiso agarrar
-  .then(res => {
-    return res.json();
-  })
-  .then(res => {
-    let results = res.results;
-    results.forEach(result => {
-      resultado.innerHTML += `
-      <div class="item" id="card">
-      <div class="card">
-      <h3 class="${result.type}">${result.type}: ${result.title.substring(0,37)}</h3>
-      <img src="${result.image_url}">
-      <p>${result.synopsis.substring(0,35)}...</p>
-      <span class="episodio"><strong> Episodes:</strong> ${result.episodes}</span>
-      </div>
-      </div>`;
-    });
-  })
-  .catch(err =>  alert("Error: " + err) );
-});
-
-function mostrar(resultador) {
+  for (var i = 0; i < res.length; i++) {
+    resultado.innerHTML += `
+    <div id="respuesta" class="contenedor-tarjeta">
+      <figure>
+        <img src="${res[i].image_url}" alt="" class="frontal">
+        <h3 class="card-type ${res[i].type}">${res[i].type}</h3>
+        <figcaption class="trasera">
+          <h3 class="card-title">${res[i].title}</h3>
+          <hr>
+          <p>${res[i].synopsis}</p>
+        </figcaption>
+      </figure>
+    `;
+  }
 
 }
