4bf8e57e9a406fc7ac8ca2e305272b9364d149c3
VPJose <josegregoriovp@gmail.com>
2020-05-06 23:30:43 -0400

    agregando efectos a la tarjeta

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-//Variables
+//VARIABLES
 var formulaio = document.getElementById("Formulario");
 var resultado = document.getElementById('resultado');
 
+//EVENTOS
 formulaio.addEventListener('submit', e => {
   e.preventDefault();
   var bsq = document.querySelector('.search').value;
@@ -16,8 +17,41 @@ formulaio.addEventListener('submit', e => {
   .catch( err =>  alert( "Error: " + err) );
 });
 
+resultado.addEventListener('click', e => {
+  e.preventDefault();
+
+  const tarjeta = e.target.parentElement;
+  const pantalla = e.target.parentElement.parentElement;
+
+  if ((pantalla.className == "pantalla") && (tarjeta.tagName == "FIGURE")) {
+    tarjeta.classList.add("voltiar");
+    pantalla.classList.add("activo");
+  }
+
+});
+
+window.addEventListener('click', e => {
+
+  const close = e.target;
+  const ventana = e.target;
+  if (close.className == "close") {
+    const tarjeta = close.parentElement.parentElement;
+    const pantalla = tarjeta.parentElement;
+    console.log(tarjeta);
+    console.log(pantalla);
+    tarjeta.classList.remove("voltiar");
+    pantalla.classList.remove("activo");
+  }else if (ventana.className == "pantalla activo") {
+    const tarjeta = ventana.firstElementChild;
+    const pantalla = ventana;
+    tarjeta.classList.remove("voltiar");
+    pantalla.classList.remove("activo");
+  }
+
+});
+
+//FUNCIONES
 function mostrar(res) {
-  console.log(res[0]);
   var respuesta = document.getElementById('respuesta');
 
   while (respuesta) {
@@ -26,17 +60,22 @@ function mostrar(res) {
   }
 
   for (var i = 0; i < res.length; i++) {
+
     resultado.innerHTML += `
     <div id="respuesta" class="contenedor-tarjeta">
-      <figure>
-        <img src="${res[i].image_url}" alt="" class="frontal">
-        <h3 class="card-type ${res[i].type}">${res[i].type}</h3>
-        <figcaption class="trasera">
-          <h3 class="card-title">${res[i].title}</h3>
-          <hr>
-          <p>${res[i].synopsis}</p>
-        </figcaption>
-      </figure>
+      <div class="pantalla">
+        <figure>
+          <img src="${res[i].image_url}" alt="" class="frontal">
+          <h3 class="card-type ${res[i].type}">${res[i].type}</h3>
+          <figcaption class="trasera">
+            <div id="close" class="close">X</div>
+            <h3 class="card-title">${res[i].title}</h3>
+            <hr>
+            <p>${res[i].synopsis}</p>
+          </figcaption>
+        </figure>
+      </div>
+    </div>
     `;
   }
 
